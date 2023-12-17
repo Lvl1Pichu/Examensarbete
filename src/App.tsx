305af276bc2ae2fcee9785b5959b6fc091f095bb
Linus Hammarberg <Linus.hammarberg@gmail.com>
2023-12-17 18:28:15 +0100

    Installed React for project template

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>
+          <p>hej din apa</p>
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
