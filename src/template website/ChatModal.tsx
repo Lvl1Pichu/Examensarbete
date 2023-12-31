43b311bacbcc4108884c90f703064d192e655139
Linus Hammarberg <Linus.hammarberg@gmail.com>
2023-12-31 11:25:17 +0100

    Created modal with errors. Commit to keep working on other units.

--- src/template website/ChatModal.tsx
+++ src/template website/ChatModal.tsx
@@ -1,16 +1,39 @@
 import {
-  CometChatMessageComposer,
   CometChatMessageHeader,
   CometChatMessages,
+  CometChatMessageComposer,
 } from "@cometchat/chat-uikit-react";
-import React from "react";
+import styled from "styled-components";
 
-export const ChatModal: React.FC = () => {
+export const ChatModal = () => {
   return (
-    <>
-      <CometChatMessageHeader></CometChatMessageHeader>
-      <CometChatMessages />
-      <CometChatMessageComposer></CometChatMessageComposer>
-    </>
+    <ChatContainer>
+      <CometChatMessageHeader />
+      <ChatMessagesContainer>
+        <CometChatMessages />
+      </ChatMessagesContainer>
+      <ChatComposerContainer>
+        <CometChatMessageComposer />
+      </ChatComposerContainer>
+    </ChatContainer>
   );
 };
+
+const ChatContainer = styled.div`
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  max-width: 400px;
+  width: 100%;
+`;
+
+const ChatMessagesContainer = styled.div`
+  max-height: 300px;
+  overflow-y: auto;
+`;
+
+const ChatComposerContainer = styled.div`
+  padding: 10px;
+`;
+
+export default ChatModal;
