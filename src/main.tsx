fced2d7691cf367925da5c730940fdab264dce53
Linus Hammarberg <Linus.hammarberg@gmail.com>
2023-12-31 10:49:13 +0100

    Routing slutförd.

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,24 @@ import App from "./App.tsx";
 import "./index.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { HomePage } from "./template website/Pages/HomePage.tsx";
-import Login from "./template website/Login.tsx";
 import Register from "./template website/Register.tsx";
+import Login from "./template website/LogIn.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route index element={<HomePage />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <App />
+              <Routes>
+                <Route index element={<HomePage />} />
+              </Routes>
+            </>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
