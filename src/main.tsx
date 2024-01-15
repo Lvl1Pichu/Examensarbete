8adcffc8d36c0a4598feb0c3b5829b417aed3420
Linus Hammarberg <Linus.hammarberg@gmail.com>
2024-01-15 13:21:25 +0100

    Made isAuthorized state into local storage to maintain state across platform

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import { HomePage } from "./template website/Pages/HomePage.tsx";
 import Login from "./template website/Pages/LogInPage.tsx";
 import { CometChatProvider } from "./CometChat/CometChatContext.tsx";
 import { SupportContextProvider } from "./Support Engine/MessageContext.tsx";
+import { SupportPage } from "./template website/Pages/SupportPage.tsx";
+
+interface PrivateRouteProps {
+  children: React.ReactNode;
+}
+
+// PrivateRoute component
+// eslint-disable-next-line react-refresh/only-export-components
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
+  const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -21,11 +38,19 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
                   <App />
                   <Routes>
                     <Route index element={<HomePage />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route
+                      path="/supportPage"
+                      element={
+                        <PrivateRoute>
+                          <SupportPage />
+                        </PrivateRoute>
+                      }
+                    />
                   </Routes>
                 </>
               }
             />
-            <Route path="/login" element={<Login />} />
           </Routes>
         </Router>
       </SupportContextProvider>
